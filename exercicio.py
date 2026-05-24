abd46678f25c25eec145ec4fe30958d5bc24c5e7
Felipe Nascimento <felipenascimento871@gmail.com>
2026-05-23 23:56:25 -0300

    resolvido o quinto exercício.

--- exercicio.py
+++ exercicio.py
@@ -59,6 +59,12 @@ else:
 # transações suspeitas. Uma transação é considerada suspeita se o valor for superior 
 # a R$ 10.000 ou se ocorrer fora do horário comercial (antes das 9h ou depois das 18h). 
 # Dada uma transação como `transacao = {'valor': 12000, 'hora': 20}`, verifique se ela é suspeita.
+transacao = {'valor': 12000, 'hora': 20}
+
+if transacao['valor'] > 10000 or transacao['hora'] < 9 or transacao['hora'] > 18:
+    print('Transação suspeita')
+else:
+    print('Tramsação normal')
 
 ### Exercício 6. Contagem de Palavras em Textos
 # Objetivo:** Dado um texto, contar quantas vezes cada palavra única aparece nele.
